bcb5a89c8dee5513ed0e260475574ac17dbb5c46
BhusanGhimire <129292830+BhusanGhimire@users.noreply.github.com>
2023-09-24 09:08:37 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const input = document.querySelector('section #input input');
 const search = document.querySelector('section #input #search')
-
 const output_div = document.querySelector('#output')
-let userInput;
+
+let page;
 
 search.addEventListener('click', e => {
-userInput = input.value || 'nature';
-page = Math.floor(Math.random()*10)
- main();
+ main(input.value || 'nature');
+})
+
+document.addEventListener('keypress', e => {
+    if(e.key == 'Enter'){
+        main(input.value || 'nature');
+    }
 })
 
 
-async function main(){
+async function main(userInput){
+    page = Math.floor(Math.random()*10);
+
+    [...output_div.children].forEach(elem => {
+        elem.remove()
+    })
+    
     let url=`https://api.unsplash.com/search/photos?page=${page}&query=${userInput}&client_id=Oi972sN9-1Edt1RuRzaPanQ2XV-bz7qJ7lqXyItd5bc`
     let response = await fetch(url)
     let data = await response.json() 
@@ -19,10 +29,7 @@ async function main(){
 
     data.results.forEach(result => {
         link = result.urls.regular
-        let img = document.createElement('img')
-        img.className = 'outputSections'
-        img.src = link
-        output_div.appendChild(img)
+        output_div.innerHTML += `<div class='outputSections'><img src=${link}></img></div>`
     });
 }
 
